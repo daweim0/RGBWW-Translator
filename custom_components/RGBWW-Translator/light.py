01183020e73cfe5dfddf9940a64723208ce8cb8e
David Michelman <dabmi@amazon.com>
2025-11-29 00:49:56 -0800

    spaghetti against the wall

--- custom_components/RGBWW-Translator/light.py
+++ custom_components/RGBWW-Translator/light.py
@@ -1,5 +1,22 @@
 from __future__ import annotations
 
+import logging
+
+
+_LOGGER = logging.getLogger("RGBWW-Translator")
+_LOGGER.warning("importing light.py")
+
+
+import voluptuous as vol
+
+# Import the device class from the component that you want to support
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.light import (ATTR_BRIGHTNESS, PLATFORM_SCHEMA,
+                                            LightEntity)
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
 
 from homeassistant.components.light import (
     SUPPORT_COLOR_TEMP,
@@ -12,13 +29,28 @@ from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity import DeviceInfo
 
 
-DOMAIN = "rgbww_translator"
+# Validation of the user's configuration
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required("name"): cv.string,
+    vol.Required("target_light"): cv.string,
+})
 
 
-async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities):
-    target_light = entry.data.get("target_light")
-    name = entry.data.get("name", "Fake Proxy Light")
-    async_add_entities([FakeProxyLight(hass, name, target_light)])
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None
+) -> None:
+    """Set up the Awesome Light platform."""
+    # Assign configuration variables.
+    # The configuration check takes care they are present.
+    _LOGGER.error("asdf")
+    name = config["name"]
+    target_light = config["target_light"]
+    # Add devices
+    add_entities([FakeProxyLight(hass, name, target_light)])
+
 
 
 class FakeProxyLight(LightEntity):
@@ -50,10 +82,13 @@ class FakeProxyLight(LightEntity):
     def color_temp(self):
         return self._color_temp
 
-    async def async_turn_on(self, **kwargs):
+    def turn_on(self, **kwargs):
+        _LOGGER.warn("turn_on() called with {}".format(kwargs))
         data = {"entity_id": self._target}
         if ATTR_BRIGHTNESS in kwargs:
             data[ATTR_BRIGHTNESS] = kwargs[ATTR_BRIGHTNESS]
             self._brightness = kwargs[ATTR_BRIGHTNESS]
         if ATTR_COLOR_TEMP in kwargs:
             data[ATTR_COLOR_TEMP] = kwargs[ATTR_COLOR_TEMP]
+
+
